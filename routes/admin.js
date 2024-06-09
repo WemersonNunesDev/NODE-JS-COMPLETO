a64828c4ff79fb503d7c30ad2e0adc68fdec88c6
WemersonNunesDev <wemersononunes@gmail.com>
2024-06-09 16:57:31 -0300

    Uppdate
    Correção

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('admin-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+  res.render('add-products', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 // /admin/add-product => POST
