23af8691ed18a3df61d607f47b09c0dcbd8e9e0a
WemersonNunesDev <wemersononunes@gmail.com>
2024-06-25 16:33:49 -0300

    Novas Rotas
    Criando novas rotas para a loja online

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,8 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 //página -> admin.js
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-products', {
+    res.render('admin/add-products', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -13,21 +13,21 @@ exports.getAddProduct = (req, res, next) => {
 
 //página -> admin.js
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
 
-//página -> shop.js
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
         });
     });
 };
--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,7 @@
 //página -> app.js
 exports.get404 = (req, res, next) => {
     res.status(404).render('404', {
-        pageTitle: 'Page Not Found!'
+        pageTitle: 'Page Not Found!',
+        path: '/404'
     });
 };
